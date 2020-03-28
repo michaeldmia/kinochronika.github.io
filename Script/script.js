7244352918766410c2724ba19f9ad6274b11f6e0
Michael Miano <m_miano@massbay.edu>
2020-03-28 16:39:05 -0400

    Add parallax banners

--- Script/script.js
+++ Script/script.js
@@ -19,3 +19,19 @@ function stickyNav() {
     navbar.classList.remove("sticky");
   }
 }
+
+function parallax() {
+	var $slider = document.getElementById("slider");
+
+	var yPos = window.pageYOffset / $slider.dataset.speed;
+	yPos = -yPos;
+	
+	var coords = '0% '+ yPos + 'px';
+	
+	$slider.style.backgroundPosition = coords;
+}
+
+window.addEventListener("scroll", function(){
+	parallax();	
+});
+
